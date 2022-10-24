cfb11523e2a179205a21813ec71467af19084061
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-10-23 21:25:53 -0300

    Create Chute.java

---
+++ Chute.java
@@ -0,0 +1,42 @@
+package Prova;
+
+public class Chute {
+	private int positionx, positiony;
+	private int quadrante;
+	private int id;
+	private int força;
+			
+			
+	
+	public int getPositionx() {
+		return positionx;
+	}
+	public void setPositionx(int positionx) {
+		this.positionx = positionx;
+	}
+	public int getPositiony() {
+		return positiony;
+	}
+	public void setPositiony(int positiony) {
+		this.positiony = positiony;
+	}
+	public int getQuadrante() {
+		return quadrante;
+	}
+	public void setQuadrante(int quadrante) {
+		this.quadrante = quadrante;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getForça() {
+		return força;
+	}
+	public void setForça(int força) {
+		this.força = força;
+	}
+	
+}
